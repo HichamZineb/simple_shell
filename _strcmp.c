a16f875a9e3541a056db27027f1f5162c3a094d9
HichamZineb <hichamelamine@hotmail.com>
2023-05-15 10:06:20 -0700

    update _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -1,9 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: First string - Second string
+ */
 int _strcmp(char *s1, char *s2)
+
 {
-    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-    {
-        s1++;
-        s2++;
-    }
-    return *s1 - *s2;
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
