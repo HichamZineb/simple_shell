f1e4e4a4c96e13f8b248ea57235655bd0b22c6b0
KARRACH <karrach.abdellah.dev@gmail.com>
2023-05-15 12:57:10 +0100

    update

--- shell.c
+++ shell.c
@@ -6,6 +6,8 @@
 
 #define MAX_ARGS 30
 
+extern char **environ;
+
 void write_str(int fd, const char *str)
 {
 write(fd, str, strlen(str));
@@ -15,9 +17,9 @@ int main(void)
 {
 char *line = NULL;
 size_t line_size = 0;
-char *argv[MAX_ARGS];
-int argc;
-char *path;
+char *args[MAX_ARGS];
+int arg_count;
+char *path = NULL;
 char *path_copy;
 char *dir;
 char cmd_path[1024];
@@ -26,49 +28,66 @@ while (1)
 {
 write_str(STDOUT_FILENO, ":) ");
 
-if(getline(&line, &line_size, stdin) == -1)
+if (getline(&line, &line_size, stdin) == -1)
 {
 write_str(STDOUT_FILENO, "\n");
 exit(0);
 }
 
-argc = 0;
-argv[argc] = strtok(line, " \n");
+arg_count = 0;
+args[arg_count] = strtok(line, " \n");
 
-while (argv[argc] != NULL && argc < MAX_ARGS - 1)
+while (args[arg_count] != NULL && arg_count < MAX_ARGS - 1)
 {
-argc++;
-argv[argc] = strtok(NULL, " \n");
+arg_count++;
+args[arg_count] = strtok(NULL, " \n");
 }
 
-if (argv[0] == NULL)
+if (args[0] == NULL)
 {
 continue;
 }
 
-if(strcmp(argv[0], "exit") == 0)
+if (strcmp(args[0], "exit") == 0)
 {
     exit(0);
 }
 
-if (argv[0][0] == '/')
+if (strcmp(args[0], "env") == 0)
 {
-
-if(access(argv[0], X_OK) == 0)
-{
-strcpy(cmd_path, argv[0]);
+    for (int i = 0; environ[i] != NULL; i++)
+    {
+        write_str(STDOUT_FILENO, environ[i]);
+        write_str(STDOUT_FILENO, "\n");
+    }
+    continue;
 }
 
+if (args[0][0] == '/')
+{
+
+if (access(args[0], X_OK) == 0)
+{
+strcpy(cmd_path, args[0]);
+}
 else
 {
-write_str(STDERR_FILENO, argv[0]);
+write_str(STDERR_FILENO, args[0]);
 write_str(STDERR_FILENO, ": command not found\n");
 continue;
 }
 }
 else
 {
-path = getenv("PATH");
+//remplece getenv
+for (int i = 0; environ[i] != NULL; i++)
+{
+    if (strncmp(environ[i], "PATH=", 5) == 0)
+    {
+        path = environ[i] + 5;
+        break;
+    }
+}
 if (path == NULL)
 {
 write_str(STDERR_FILENO, "PATH environment not set\n");
@@ -81,8 +100,8 @@ while (dir != NULL)
 {
 strcpy(cmd_path, dir);
 strcat(cmd_path, "/");
-strcat(cmd_path, argv[0]);
-if(access(cmd_path, X_OK) == 0)
+strcat(cmd_path, args[0]);
+if (access(cmd_path, X_OK) == 0)
 {
 break;
 }
@@ -91,15 +110,15 @@ dir = strtok(NULL, ":");
 free(path_copy);
 if (dir == NULL)
 {
-write_str(STDERR_FILENO, argv[0]);
+write_str(STDERR_FILENO, args[0]);
 write_str(STDERR_FILENO, ": command not found\n");
 continue;
 }
 }
 if (fork() == 0)
 {
-execve(cmd_path, argv, NULL);
-perror(argv[0]);
+execve(cmd_path, args, NULL);
+perror(args[0]);
 exit(0);
 }
 else
