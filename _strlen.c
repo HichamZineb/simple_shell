6fd6517a802869d3b9173e7578ab09abce5dbe2a
HichamZineb <hichamelamine@hotmail.com>
2023-05-15 10:17:11 -0700

    update _strlen.c

--- _strlen.c
+++ _strlen.c
@@ -1,10 +1,20 @@
-size_t _strlen(char *str)
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: String to mesure
+ *
+ * Return: Length of string
+ */
+size_t _strlen(const char *str)
+
 {
-    size_t len = 0;
-    while (*str != '\0')
-    {
-        len++;
-        str++;
-    }
-    return len;
+	size_t len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
 }
