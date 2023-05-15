e80e0538a9bf6b74c9e896d2563290fbf56b51e7
HichamZineb <hichamelamine@hotmail.com>
2023-05-15 10:09:11 -0700

    update _strdup.c

--- _strdup.c
+++ _strdup.c
@@ -1,11 +1,25 @@
+#include "shell.h"
+
+/**
+ * _strdup - Duplicate string
+ * @str: String to duplicate
+ *
+ * Return: Duplicate of the original string
+ */
 char *_strdup(char *str)
+
 {
-    int len = strlen(str);
-    char *new_str = malloc(len + 1);
-    if (new_str == NULL)
-        return NULL;
-    for (int i = 0; i < len; i++)
-        new_str[i] = str[i];
-    new_str[len] = '\0';
-    return new_str;
+	int i;
+	int len = strlen(str);
+	char *new_str = malloc(len + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		new_str[i] = str[i];
+
+	new_str[len] = '\0';
+
+	return (new_str);
 }
