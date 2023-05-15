8b687007e14d8b31c528c89c4003ccbed6b5d425
HichamZineb <hichamelamine@hotmail.com>
2023-05-15 10:07:44 -0700

    update _strcpy.c

--- _strcpy.c
+++ _strcpy.c
@@ -1,12 +1,24 @@
+#include "shell.h"
+
+/**
+ * _strcpy - Copies from source to destination
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Temp
+ */
 char *_strcpy(char *dest, char *src)
+
 {
-    char *temp = dest;
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return temp;
+	char *temp = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (temp);
 }
