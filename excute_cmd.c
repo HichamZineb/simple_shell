5d33ae955be81a2764a58c860d54c98e4751e424
KARRACH <karrach.abdellah.dev@gmail.com>
2023-05-24 23:33:56 +0100

    tst

--- excute_cmd.c
+++ excute_cmd.c
@@ -7,7 +7,7 @@
  */
 void execute_command(char *cmd_path, char **argv)
 {
-if (_strcmp(argv[0], "exit") != 0
+if (cmd_path != NULL && _strcmp(argv[0], "exit") != 0
 && _strcmp(argv[0], "setenv") != 0
 && _strcmp(argv[0], "unsetenv") != 0 && _strcmp(argv[0], "cd") != 0)
 {
